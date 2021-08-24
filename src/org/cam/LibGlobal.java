acd1bb1d6c5aacf92b429e4cdf7233b09ad1393c
kavyaneeraj <kavyagoalla26@gmail.com>
2021-08-24 12:38:46 +0530

    loadUrl Done

--- src/org/cam/LibGlobal.java
+++ src/org/cam/LibGlobal.java
@@ -1,5 +1,16 @@
 package org.cam;
 
-public class LibGlobal {
 
+public class LibGlobal {
+	
+	public void loadUrl() {
+
+System.out.println("LoadUrl");
+	
+	}
+
+public static void main(String[] args) {
+	LibGlobal l = new LibGlobal();
+	l.loadUrl();
+}
 }
